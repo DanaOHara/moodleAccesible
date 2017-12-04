d9d220904ed80e273a807cd0b356010419bc9f4f
DanaOHara <rosa.munozv@usach.cl>
2017-12-04 00:56:48 -0300

    pagina inicial y redireccion al login ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
 
   resources :mdl_events
   resources :mdl_forum_posts
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   resources :mdl_courses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :mdl_users
+
+
+get 'mdl_users/', to: 'mdl_users#index'
 
   get 'mdl_forums/obtenerForosPorCurso/:course', to: 'mdl_forums#obtenerForosPorCurso'
   get 'mdl_forum_discussions/obtenerDiscusionesPorForo/:forum', to: 'mdl_forum_discussions#obtenerDiscusionesPorForo'
