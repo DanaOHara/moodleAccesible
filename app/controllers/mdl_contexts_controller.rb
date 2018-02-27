1182855c9cb20f297cb5e9886a217308123be8b0
DanaOHara <rosa.munozv@usach.cl>
2018-02-27 11:34:00 -0300

    se colo un 7 fugitivo

--- app/controllers/mdl_contexts_controller.rb
+++ app/controllers/mdl_contexts_controller.rb
@@ -14,69 +14,10 @@ render json: @mdl_context
 end
 
 
-
-  def curso
-
-  @cusos = MdlContext.select(:fullname).joins(" INNER JOIN mdl_role_assignments  ra ON  ra.contextid =  mdl_context.id INNER JOIN mdl_course co ON co.id = mdl_context.instanceid" ).where("ra.userid = ? ", params[:userid])
-
-  render json: @cursos
-
-  end
-
-
-  # GET /mdl_contexts/1
-  # GET /mdl_contexts/1.json
   def show
   end
 
-  # GET /mdl_contexts/new
-  def new
-    @mdl_context = MdlContext.new
-  end
 
-  # GET /mdl_contexts/1/editINNER JOIN
-  def edit
-  end
-
-  # POST /mdl_contexts
-  # POST /mdl_contexts.json
-  def create
-    @mdl_context = MdlContext.new(mdl_context_params)
-
-    respond_to do |format|
-      if @mdl_context.save
-        format.html { redirect_to @mdl_context, notice: 'Mdl context was successfully created.' }
-        format.json { render :show, status: :created, location: @mdl_context }
-      else
-        format.html { render :new }
-        format.json { render json: @mdl_context.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /mdl_contexts/1
-  # PATCH/PUT /mdl_contexts/1.json
-  def update
-    respond_to do |format|
-      if @mdl_context.update(mdl_context_params)
-        format.html { redirect_to @mdl_context, notice: 'Mdl context was successfully updated.' }
-        format.json { render :show, status: :ok, location: @mdl_context }
-      else
-        format.html { render :edit }
-        format.json { render json: @mdl_context.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /mdl_contexts/1
-  # DELETE /mdl_contexts/1.json
-  def destroy
-    @mdl_context.destroy
-    respond_to do |format|
-      format.html { redirect_to mdl_contexts_url, notice: 'Mdl context was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
